53360a037eea33b9ab85573b08a1764a640120ee
ktbm <boliventtalla12@gmail.com>
2026-04-14 09:12:45 +0100

    update

--- constructions/urls.py
+++ constructions/urls.py
@@ -6,4 +6,8 @@ from constructions import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('projects/', views.projects, name='projects'),
+    
 ]
--- constructions/views.py
+++ constructions/views.py
@@ -6,3 +6,11 @@ def index(request):
 
 def contact(request):
     return render(request, 'contact.html')
+def about(request):
+    return render(request, 'about.html')
+def services(request):
+    return render(request, 'services.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+ 
